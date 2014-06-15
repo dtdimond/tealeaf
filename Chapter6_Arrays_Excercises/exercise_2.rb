7bbde72ab19a3f1f3d03980d537bbb48f48ef266
Dan Dimond <dtdimond@gmail.com>
2014-06-15 14:37:58 +1000

    Arrays

---
+++ Chapter6_Arrays_Excercises/exercise_2.rb
@@ -0,0 +1,5 @@
+#arr value = [["b"],["b",2],["b",3],["a",1],["a",2],["a",3]]
+#returns 1 (the value deleted)
+#
+#arr value = ["b",["a",[1,2,3]]]
+#returns [1,2,3] (the value deleted)
---
+++ Chapter6_Arrays_Excercises/exercise_7.rb
@@ -0,0 +1,9 @@
+arr = [2,5,88]
+arr2 = []
+
+arr.each do |n|
+  arr2.push(n + 2)
+end
+
+p arr
+p arr2
